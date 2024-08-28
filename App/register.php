9c29815fac132d10e6884d1bf25b5f57e97987a0
Colby Berger <60939024+colbyx20@users.noreply.github.com>
2024-08-27 23:23:40 -0400

    Update register.php

--- App/register.php
+++ App/register.php
@@ -1,7 +1,7 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+  header('Access-Control-Allow-Origin: *');
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 	error_reporting(E_ALL); // reports all errors
 	ini_set("display_errors", "1"); // shows all errors
